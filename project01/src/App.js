11c3be49d7b2884ae371cc5fb383738b321aa45b
Kim-woo-gyeong <lovelykwg@naver.com>
2020-05-01 14:05:01 +0900

    react #1

---
+++ project01/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyComponent from './MyComponent.js';
+
+const App = () => {
+    return (
+        <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
+    );
+}
+
+export default App;
