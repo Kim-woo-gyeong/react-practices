d79586e18e90f7a48aff2e9f93b853fed6f90874
Kim-woo-gyeong <lovelykwg@naver.com>
2020-05-02 17:17:23 +0900

    react #Dom

---
+++ project-sample/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BootStrap from './BootStrap';
+
+const App = () => {
+    return (
+        <BootStrap />
+    );
+}
+
+export default App;
--- project03/src/App.js
+++ project03/src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-// import MyComponent from './MyComponent.js';
-// import Counter from './Counter';
-// import Say from './Say';
-import EventPractice from './EventPractice';
+// import ValidationSample from './ValidationSample'
+import ScrollBox from './ScrollBox';
 
 const App = () => {
     return (
-        // 1. component 예제
-        // <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
-        
-        //2. state 예제
-        // <Counter />
-
-        //3. useState 예제
-        // <Say />
-
-        //4. 이벤트 핸들러 예제
-        <EventPractice />
+        <div>
+            {/* <ValidationSample /> */}
+            <ScrollBox ref={(ref) => this.scrollBox=ref} />
+            <button onClick={() => this.scrollBox.scrollTopBottom()}>
+                맨밑으로
+            </button>
+        </div>
     );
 }
 
